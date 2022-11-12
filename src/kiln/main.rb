458a50c17df18e3688bb24415a1c71b49dacd3ea
montyz <monty@codetransform.com>
2022-11-11 22:34:54 -0500

    added method for file reloading

--- src/kiln/main.rb
+++ src/kiln/main.rb
@@ -1,12 +1,13 @@
 require "sketchup.rb"
 
+# Monty::KilnTool.reloadFiles
 module Monty
   module KilnTool
     def self.create_brick
       l = 9
       w = 4.5
       h = 2.5
-      n = 20
+      n = 5
       s = 10
       model = Sketchup.active_model
       model.start_operation("Create Kiln", true)
@@ -28,6 +29,18 @@ module Monty
       model.commit_operation
     end
 
+    def self.reloadFiles
+      original_verbose = $VERBOSE
+      $VERBOSE = nil
+      pattern = File.join(__dir__, "**/*.rb")
+      Dir.glob(pattern).each { |file|
+        # Cannot use `Sketchup.load` because its an alias for `Sketchup.require`.
+        load file
+      }.size
+    ensure
+      $VERBOSE = original_verbose
+    end
+  
     unless file_loaded?(__FILE__)
       menu = UI.menu("Plugins")
       menu.add_item("Create Kiln") {
