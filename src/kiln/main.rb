5336c432b2fce158c0b76df7053d88857d7bc9cb
montyz <monty@codetransform.com>
2022-11-30 13:43:59 -0800

    progress on course 4

--- src/kiln/main.rb
+++ src/kiln/main.rb
@@ -45,7 +45,7 @@ module Monty
       # col 0
       bx = 0
       by = 0
-      19.times do |i|
+      20.times do |i|
         lay_brick('IFB', bx, (i * 2) + by)
       end
       # col 1
@@ -54,7 +54,7 @@ module Monty
       by += 2
       lay_brick('FB/2', bx, by)
       by += 1
-      17.times do |i|
+      18.times do |i|
         lay_brick('FB', bx, (i * 2) + by)
       end
       by += 17 * 2
@@ -91,7 +91,7 @@ module Monty
       lay_brick('FB', bx, by + 3)
       lay_brick('FB/2', bx, by + 5)
       by = 8
-      14.times do |i|
+      16.times do |i|
         lay_brick('FB', bx, (i * 2) + by)
       end
       # col 9
@@ -110,7 +110,7 @@ module Monty
       lay_brick('IFB', bx, by + 11)
       lay_brick('FB', bx, by + 13)
       by += 15
-      7.times do |i|
+      8.times do |i|
         lay_brick('IFB', bx, (i * 2) + by)
       end
       @height += 2.5
@@ -120,6 +120,8 @@ module Monty
       (2..7).each do |bx|
         lay_brick('FB/2', bx, 29)
         lay_brick('FB', bx, 30)
+        lay_brick('FB', bx, 36)
+        lay_brick('FB/2', bx, 38)
       end
     end
 
@@ -170,8 +172,8 @@ module Monty
       l = componentdefinition.bounds.height
       w = componentdefinition.bounds.width
       5.times do |i|
-        10.times do |j|
-          transformation = Geom::Transformation.new([i * (w + (5.0 / 4.0)), j * (l + (11.0 / 9.0)), @height])
+        11.times do |j|
+          transformation = Geom::Transformation.new([i * (w + (5.0 / 4.0)), j * (l + (4.0 / 10.0)), @height])
           componentinstance = entities.add_instance(componentdefinition, transformation)
           componentinstance.material = componentdefinition.material
           componentinstance.layer = @layer
@@ -192,7 +194,7 @@ module Monty
       degrees_to_rotate = 90.degrees
       t = Geom::Transformation.rotation(target_point, vector, degrees_to_rotate)
       5.times do |i|
-        38.times do |j|
+        40.times do |j|
           transformation = Geom::Transformation.new([(i * w) + w, j * l, @height]) * t
           componentinstance = entities.add_instance(componentdefinition, transformation)
           componentinstance.material = componentdefinition.material
@@ -209,7 +211,7 @@ module Monty
       l = componentdefinition.bounds.height
       w = componentdefinition.bounds.width
       5.times do |i|
-        19.times do |j|
+        20.times do |j|
           transformation = Geom::Transformation.new([i * w, j * l, @height])
           componentinstance = entities.add_instance(componentdefinition, transformation)
           componentinstance.material = componentdefinition.material
@@ -241,8 +243,8 @@ module Monty
       create_brick(l / 2, w, h, 'IFB/2', 'Cornsilk')
       create_brick(l / 4, w, h, 'FB/4', 'Goldenrod')
       create_brick(l / 4, w, h, 'IFB/4', 'Cornsilk')
-      create_brick(16.0, 8.0, 8.0, 'Cinder Block', 'LightSlateGray')
-      create_brick(171.0, 45.0, 3.5, 'Slab', 'LightSlateGray')
+      create_brick(16.0, 8.0, 8.0, 'Cinder Block', 'SlateGray')
+      create_brick(180.0, 45.0, 3.5, 'Slab', 'LightSlateGray')
     end
 
     def self.find_componentdefinition(name)
